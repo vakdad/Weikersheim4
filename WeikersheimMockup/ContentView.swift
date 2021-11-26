860faaa9c4b28cf0a275d64c4ea3e7c3fb7ab481
Geoffry Wharton <g231231@gmail.com>
2021-11-26 19:40:39 +0100

    Initial Commit

---
+++ WeikersheimMockup/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WeikersheimMockup
+//
+//  Created by Geoffry Wharton on 26.11.21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ WeikersheimMockup/WeikersheimMockupApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeikersheimMockupApp.swift
+//  WeikersheimMockup
+//
+//  Created by Geoffry Wharton on 26.11.21.
+//
+
+import SwiftUI
+
+@main
+struct WeikersheimMockupApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
